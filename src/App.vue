b20f9f5e2359d740dfb57d21d0588ef7c23bd1c4
jplascencia47 <jplascencia@unomaha.edu>
2025-09-17 23:26:51 -0500

    Part 2 Front End/VIDEO
    Front End Code with VIdeo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<!--<router-view/> required to route to views - add to each template unless included here-->
+<router-view/>
 </template>
 
-<style>
-#app {
+<script>
+import router from "./router";
+export default {
+ name: 'app',
+ props: {
+          msg: String
+      },
+  mounted(){
+    router.push("/skill-list");
+  }
+}
+</script>
+
+<style lang="scss">
+ #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,40 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+ import SkillList from '../components/SkillList'
+ import SkillCreate from '../components/SkillCreate'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'SkillList',
+    component: SkillList,
+  },
+   {
+     path: '/skill-list',
+     name: 'SkillList',
+     component: SkillList,
+   },
+   {
+     path:'/skill-list/:msg',
+     name: 'SkillUpdatedList',
+     component: SkillList
+   },
+   {
+    path:'/skill-create',
+    name:'SkillCreate',
+    component: SkillCreate
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/skill-create/:pk',
+    name: 'SkillUpdate',
+    component: SkillCreate
   }
-]
+
+ ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+   history: createWebHistory(process.env.BASE_URL),
+   routes
+ })
 
 export default router
+
